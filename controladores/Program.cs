25662829f36f5c0efc038a86cbbfcfe37029f026
Jesús <csi22-jalvugo@colegioaltair.net>
2023-10-24 11:47:33 +0200

    Agregar archivos de proyecto.

---
+++ controladores/Program.cs
@@ -0,0 +1,25 @@
+﻿using ejercicio1Condi.servicios;
+
+namespace ejercicio1Condi.controladores
+{
+    class Program
+    {
+        internal static void Main(string[] args)
+        {
+            int numero;
+            operadorInterfaz mi = new operadorImplementacion();
+
+            numero = mi.PedirOperador();
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número es par");
+            }
+
+            else
+            {
+                Console.WriteLine("El número introducido es impar");
+            }
+        }
+    }
+}
---
+++ servicios/operadorImplementacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1Condi.servicios
+{
+    internal class operadorImplementacion : operadorInterfaz
+    {
+        public int PedirOperador()
+        {
+            int numero;
+
+            Console.WriteLine("Introduce un número");
+            numero = Int32.Parse(Console.ReadLine());
+
+            return numero;
+        }
+    }
+}
